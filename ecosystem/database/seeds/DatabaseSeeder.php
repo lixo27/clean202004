6f247d3d2344cec9016c31463b1196800165531f
Thiago Senna <trsenna@gmail.com>
2020-04-19 01:12:04 -0300

    customer seeder moved

--- ecosystem/database/seeds/DatabaseSeeder.php
+++ ecosystem/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call( [
-            \Clean\Persistence\Customer\CustomerSeeder::class,
+            CustomerSeeder::class,
         ] );
     }
 }
--- package/resources/database/seeds/CustomerSeeder.php
+++ package/resources/database/seeds/CustomerSeeder.php
@@ -1,15 +1,11 @@
 <?php
 
-namespace Clean\Persistence\Customer;
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 /**
  * Class CustomerSeeder
- *
- * @package Clean\Persistence\Customer
  */
 class CustomerSeeder extends Seeder
 {
@@ -18,9 +14,9 @@ class CustomerSeeder extends Seeder
         $range = Collection::make( range( 1, 5 ) );
         $customers = $range->map( function ( $index ) {
             return [
-                'name' => "Customer Name #{$index}",
                 'created_at' => new \DateTime(),
                 'updated_at' => new \DateTime(),
+                'name' => "Customer Name #{$index}",
             ];
         } );
 
