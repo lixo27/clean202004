2a9b3d1f8642d6a4e7dee81d9ee78d27ea018877
Thiago Senna <trsenna@gmail.com>
2020-04-20 19:51:34 -0300

    factories for seed work, but needs fix about the different directory location

---
+++ package/database/factories/CustomerFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+$factory->define(
+    \Clean\Domain\Customers\CustomerModel::class,
+    function ( \Faker\Generator $faker ) {
+        return [
+            'created_at' => now(),
+            'updated_at' => now()->addDay(),
+            'name' => $faker->name,
+        ];
+    }
+);
--- package/database/seeds/CustomerSeeder.php
+++ package/database/seeds/CustomerSeeder.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
 
 /**
  * Class CustomerSeeder
@@ -11,15 +9,6 @@ class CustomerSeeder extends Seeder
 {
     public function run(): void
     {
-        $range = Collection::make( range( 1, 5 ) );
-        $customers = $range->map( function ( $index ) {
-            return [
-                'created_at' => new \DateTime(),
-                'updated_at' => new \DateTime(),
-                'name' => "Customer Name #{$index}",
-            ];
-        } );
-
-        DB::table( 'customers' )->insert( $customers->toArray() );
+        factory( \Clean\Domain\Customers\CustomerModel::class, 10 )->create();
     }
 }
