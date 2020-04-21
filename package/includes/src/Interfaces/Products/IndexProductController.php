36bb3851d62befcf808c5dc0ee2a5fc452098017
Thiago Senna <trsenna@gmail.com>
2020-04-20 23:07:51 -0300

    added products interfaces and views

---
+++ package/includes/src/Interfaces/Products/IndexProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Clean\Interfaces\Products;
+
+use Clean\Application\Customers\Queries\GetCustomerList\GetCustomerListInterface;
+use Clean\Application\Products\Queries\GetProductList\GetProductListInterface;
+use Illuminate\Routing\Controller;
+
+/**
+ * Class IndexProductController
+ *
+ * @package Clean\Interfaces\Products
+ */
+class IndexProductController extends Controller
+{
+    /**
+     * @var GetProductListInterface
+     */
+    private $productList;
+
+    /**
+     * IndexProductController constructor.
+     *
+     * @param GetProductListInterface $productList
+     */
+    public function __construct( GetProductListInterface $productList )
+    {
+        $this->productList = $productList;
+    }
+
+    public function __invoke()
+    {
+        $products = $this->productList->execute();
+
+        return view( 'product.index', [
+            'products' => $products,
+        ] );
+    }
+}
---
+++ package/resources/views/product/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('title', 'Products')
+
+@section('content')
+
+    <h1>Products</h1>
+
+    <table class="table table-bordered">
+
+        <thead class="thead-light">
+
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+        </tr>
+
+        </thead>
+
+        <tbody>
+
+        @forelse ($products as $product)
+
+            <tr>
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->price }}</td>
+            </tr>
+
+        @empty
+
+            <tr>
+                <td colspan="2">Sorry, no products</td>
+            </tr>
+
+        @endforelse
+
+        </tbody>
+
+    </table>
+
+@endsection
