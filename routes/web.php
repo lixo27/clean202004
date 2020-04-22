1fbc08ccc574f7aa0e894a4256d8cdc1545286e9
Thiago Senna <trsenna@gmail.com>
2020-04-22 16:42:11 -0300

    prefer namespacing for routes too

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+namespace Clean;
+
+use Clean\Interfaces\Customers\IndexCustomerController;
+use Clean\Interfaces\Employees\IndexEmployeeController;
+use Clean\Interfaces\Home\IndexHomeController;
+use Clean\Interfaces\Products\IndexProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get( 'clean', \Clean\Interfaces\Home\IndexHomeController::class )->name( 'home.index' );
-Route::get( 'clean/customers', \Clean\Interfaces\Customers\IndexCustomerController::class )->name( 'customer.index' );
-Route::get( 'clean/employees', \Clean\Interfaces\Employees\IndexEmployeeController::class )->name( 'employee.index' );
-Route::get( 'clean/products', \Clean\Interfaces\Products\IndexProductController::class )->name( 'product.index' );
+Route::get( 'clean', IndexHomeController::class )->name( 'home.index' );
+Route::get( 'clean/customers', IndexCustomerController::class )->name( 'customer.index' );
+Route::get( 'clean/employees', IndexEmployeeController::class )->name( 'employee.index' );
+Route::get( 'clean/products', IndexProductController::class )->name( 'product.index' );
